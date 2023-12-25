6bde7be424240532cfbfd344eda0274f135b121a
Deepak <kanshettid@gmail.com>
2023-12-25 23:23:49 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 
-import React from "react";
+import React, { useState } from 'react';
 import './../styles/App.css';
 
+// const counter =()=>{
+  
+// }
+
 const App = () => {
+  // var num = 0;
+  const [count,setCount] = useState(0);
+
+  const handleIncrement=()=>{
+    setCount(count+1);
+  }
   return (
+    
     <div>
         {/* Do not remove the main div */}
+        <p><b>Button clicked {count} times</b></p>
+        <button onClick={handleIncrement}>Click me</button>
     </div>
   )
 }
